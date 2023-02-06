99dce24031b6bdc9d06dcac39696a4a147a6530b
AnaeleJoshua <anaelejoshua@gmail.com>
2023-02-06 15:59:05 +0000

    add task 1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#print_list_integer: prints all integers of a list
+#@my_list: list argument passed
+
+def print_list_integer(my_list=[]):
+	for i in my_list:
+		print("{}".format(i))
+	return (0)
