f8294785a2d7b06c27383166cfc9cf8561b0ef74
AnaeleJoshua <anaelejoshua@gmail.com>
2023-02-18 15:49:54 +0000

    pep8 formatting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """ Task 0:Write an empty class Square that defines a square:"""
+
+
 class Square:
-	""" Empty class"""	
+
+    """ Empty class"""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Task 1: A class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module"""
+
+class Square:
+	"""  Define a square"""
+	def __init(self,size):
+		"""
+		Args:
+			size(int): the size of the square
+			"""
+		self.__size = size	
